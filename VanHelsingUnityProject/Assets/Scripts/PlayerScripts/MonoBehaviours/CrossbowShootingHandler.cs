3442254c9cb7b46872021e2f292465f84f795b82
JNeto <joaomendes15@gmail.com>
2023-04-12 01:22:39 +0100

    Main arm rotation functionalities were separated from shooting mechanics, these mechanics were moved to CrossbowShootingHandler.cs

---
+++ VanHelsingUnityProject/Assets/Scripts/PlayerScripts/MonoBehaviours/CrossbowShootingHandler.cs
@@ -0,0 +1,50 @@
+﻿using PlayerScripts.Enums;
+using UnityEngine;
+
+
+public class CrossbowShootingHandler : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject projectile;
+    private Transform shotPoint;
+    private float _shotInterval = 0.3f;
+    private float _shotCoolDownTimer = 0;
+    
+    //public Animator camAnim;
+
+
+    private void Start()
+    {
+        shotPoint = GetComponentInChildren<Transform>();
+    }
+
+    private void Update()
+    {
+        if(PlayerController.IsShooting) 
+            TryShoot();
+    }
+    
+    private void TryShoot()
+    {
+        if (Input.GetButton("Shoot") && _shotCoolDownTimer <= 0)
+        {
+            // not implemented animation
+            // camAnim.SetTrigger("shake");
+            
+            // arrow is by default facing right --> just like the crossbow
+            // I make an alternative rotation in case Van Helsing is facing left because the X scale is * -1
+            // and it messes a lot with the arrow inverting the rotation wile facing left, so I subtract 180 of it
+            Quaternion projectileRotation = transform.rotation;
+            if (PlayerController.CurrentFacingDirection == FacingDirection.Left)
+            {
+                Vector3 aux = projectileRotation.eulerAngles;
+                aux.z -= 180;
+                projectileRotation = Quaternion.Euler(aux);
+            }
+            Instantiate(projectile, shotPoint.position, projectileRotation);
+            _shotCoolDownTimer = _shotInterval;
+            return;
+        }
+        _shotCoolDownTimer -= Time.deltaTime;
+    }
+}
---
+++ VanHelsingUnityProject/Assets/Scripts/PlayerScripts/MonoBehaviours/MainArmRotationHandler.cs
@@ -0,0 +1,26 @@
+using PlayerScripts.Enums;
+using UnityEngine;
+
+
+public class MainArmRotationHandler : MonoBehaviour
+{
+
+    // Making sure that the angle in 0 is zero
+    private void Start() => transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+    void Update() => RotateArmAccordingToMousePos();
+
+    private void RotateArmAccordingToMousePos()
+    {
+        Vector3 mouseInWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 difference = mouseInWorldPos - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        
+        switch (PlayerController.CurrentFacingDirection)
+        {
+            case FacingDirection.Right: transform.rotation = Quaternion.Euler(0f, 0f, rotZ); break;
+            case FacingDirection.Left:  transform.rotation = Quaternion.Euler(0f, 0f, rotZ - 180); break;
+        }
+    }
+
+}
