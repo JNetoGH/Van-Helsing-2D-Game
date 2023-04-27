fdf7d0241c71c8706b87ffae3d907c1671d34f49
JNeto <joaomendes15@gmail.com>
2023-04-27 14:15:06 +0100

    ground dectector bug fixed

--- VanHelsingUnityProject/Assets/Scripts/PlayerScripts/PlayerMonoBehaviours/GroundSensorController.cs
+++ VanHelsingUnityProject/Assets/Scripts/PlayerScripts/PlayerMonoBehaviours/GroundSensorController.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 public class GroundSensorController : MonoBehaviour
 {
 
-    private bool isGorunded = false;
+    private int m_ColCount = 0;
     private float m_DisableTimer;
 
-    private void OnEnable() => isGorunded = false;
+    private void OnEnable() => m_ColCount = 0;
 
     public bool State()
     {
         if (m_DisableTimer > 0)
             return false;
-        return isGorunded;
+        return m_ColCount > 0;
     }
 
-    void OnTriggerEnter2D(Collider2D other) => isGorunded = true;
+    void OnTriggerEnter2D(Collider2D other) => m_ColCount++;
 
-    void OnTriggerExit2D(Collider2D other) => isGorunded = false;
+    void OnTriggerExit2D(Collider2D other) => m_ColCount--;
     
     void Update() => m_DisableTimer -= Time.deltaTime;
     
