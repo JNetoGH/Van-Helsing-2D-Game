759917c3de7f1aecf694d55050e3b7f0c4ec51ea
JNeto <joaomendes15@gmail.com>
2023-04-27 15:53:50 +0100

    new camera added

--- VanHelsingUnityProject/Assets/Scripts/Camera/CameraController.cs
+++ VanHelsingUnityProject/Assets/Scripts/Camera/CameraController.cs
@@ -1,20 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
+// Camera Smooth Spring System
 public class CameraController : MonoBehaviour
 {
-
+    
     private GameObject _player;
-    [SerializeField] private float limitInYDownWards = 0;
+    [SerializeField] private bool followInY = true;
+    [SerializeField] private bool followInX = true;
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private float speed; // Cant be bigger than 1
     
     void Start() => _player = GameObject.FindWithTag("Player");
     
-    void Update()
+    private void FixedUpdate()
     {
-        transform.position = transform.position = new Vector3(0, _player.transform.position.y, transform.position.z);
-        if (transform.position.y < limitInYDownWards)
-            transform.position = new Vector3(transform.position.x, limitInYDownWards, transform.position.z);
+        Vector3 targetPosition = _player.transform.position - new Vector3(offset.x, offset.y, 0); 
+        
+        Vector3 newPosition = transform.position;
+        Vector3 error = targetPosition - newPosition;
+        newPosition += error * speed * Mathf.Min(Time.fixedDeltaTime / speed, 1);
+        
+        if (!followInY)
+            newPosition.y = transform.position.y;
+        if (!followInX)
+            newPosition.x = transform.position.x;
+        
+        // keep z the same in order to avoid camera rendering bad distances
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
     }
-    
 }
