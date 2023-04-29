ffae4dc5a5af9a913653fdfbd6bf1412407d46ce
JNeto <joaomendes15@gmail.com>
2023-04-30 00:51:27 +0100

    amr switcher and rotation merged into arms controller

--- VanHelsingUnityProject/Assets/Scripts/PlayerScripts/PlayerMonoBehaviours/MainArmsController.cs
+++ VanHelsingUnityProject/Assets/Scripts/PlayerScripts/PlayerMonoBehaviours/MainArmsController.cs
@@ -2,18 +2,24 @@ using System.Collections.Generic;
 using PlayerScripts.Enums;
 using UnityEngine;
 
-public class MainArmsSwitcher : MonoBehaviour
+public class MainArmsController : MonoBehaviour
 {
 
     [SerializeField] private ArmState currentArmState = ArmState.Saw;
     [SerializeField] private GameObject crossbowArm;
     [SerializeField] private GameObject sawArm;
+    
     private Dictionary<ArmState, GameObject> stateAndGameObject = new Dictionary<ArmState, GameObject>();
+    private PlayerController _playerController;
     
     void Start()
     {
         stateAndGameObject.Add(ArmState.Crossbow, crossbowArm);
         stateAndGameObject.Add(ArmState.Saw, sawArm);
+        _playerController = GetComponentInParent<PlayerController>();
+        
+        // Making sure that the angle in 0 is zero
+        transform.rotation = Quaternion.Euler(0f, 0f, 0f); 
     }
     
     void Update()
@@ -29,6 +35,8 @@ public class MainArmsSwitcher : MonoBehaviour
                 case ArmState.Saw: SwitchArmState(ArmState.Crossbow); break;
             }
         }
+        
+        RotateArmAccordingToMousePos();
     }
 
     public void SwitchArmState(ArmState newState)
@@ -42,5 +50,18 @@ public class MainArmsSwitcher : MonoBehaviour
             case ArmState.Saw: SawArmController.AtkCoolDownTimer = 0; break;
         }
     }
-    
+
+    private void RotateArmAccordingToMousePos()
+    {
+        Vector3 mouseInWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 distanceToMouse = mouseInWorldPos - transform.position;
+        float rotZ = Mathf.Atan2(distanceToMouse.y, distanceToMouse.x) * Mathf.Rad2Deg;
+        
+        switch (_playerController.CurrentFacingDirection)
+        {
+            case FacingDirection.Right: transform.rotation = Quaternion.Euler(0f, 0f, rotZ); break;
+            case FacingDirection.Left:  transform.rotation = Quaternion.Euler(0f, 0f, rotZ - 180); break;
+        }
+    }
+
 }
