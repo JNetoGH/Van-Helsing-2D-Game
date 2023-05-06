d25861dbc081180c86912025802f6ee93b7e73d5
JNeto <joaomendes15@gmail.com>
2023-05-07 00:39:13 +0100

    animation system working for jump/fall/idle

--- UnityProjects/VanHelsingUnityProject/Assets/Scripts/PlayerScripts/PlayerObservers/PlayerAnimationObserver.cs
+++ UnityProjects/VanHelsingUnityProject/Assets/Scripts/PlayerScripts/PlayerObservers/PlayerAnimationObserver.cs
@@ -1,42 +1,27 @@
 using PlayerScripts.Interfaces;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace PlayerScripts.PlayerObservers
 {
     public class PlayerAnimationObserver : MonoBehaviour, IPlayerObserver
     {
     
-        [SerializeField] private Animator _spritesAnimator;
+        private Animator _animator;
         private Rigidbody2D _rb;
 
-        private void Start() => _rb = GetComponent<Rigidbody2D>();
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+        }
+
         public void OnNotifyStart(PlayerController playerController) { }
         public void OnNotifyUpdate(PlayerController playerController)
         {
-            /*// Run
-            if (Mathf.Abs(_rb.velocity.x) > 0f && Mathf.Abs(_rb.velocity.x) > playerController.maxWalkSpeed) 
-            {
-                _spritesAnimator.SetBool("IsRunning", true);
-                _spritesAnimator.SetBool("IsWalking", false);
-                _spritesAnimator.SetBool("IsInIdle", false);
-            }
-            // Walk
-            else if (Mathf.Abs(_rb.velocity.x) > 0f) 
-            {
-                _spritesAnimator.SetBool("IsRunning", false);
-                _spritesAnimator.SetBool("IsWalking", true);
-                _spritesAnimator.SetBool("IsInIdle", false);
-            }
-            // Idle
-            else 
-            {
-                _spritesAnimator.SetBool("IsRunning", false);
-                _spritesAnimator.SetBool("IsWalking", false);
-                _spritesAnimator.SetBool("IsInIdle", true);
-            }*/
-            
-            //_animator.SetBool("Grounded", IsGrounded);
+            // Jump / Fall / Grounded (Idle)
+            _animator.SetBool("IsJumping", playerController.IsJumping);
+            _animator.SetBool("IsFalling", playerController.IsFalling);
+            _animator.SetBool("IsGrounded", playerController.IsGrounded);
         }
     
     }
