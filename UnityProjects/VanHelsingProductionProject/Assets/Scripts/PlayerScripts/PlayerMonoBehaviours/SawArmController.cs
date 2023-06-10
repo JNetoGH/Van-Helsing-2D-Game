fae621c893d52d34c925317cc6b951480b6924ed
JNeto <joaomendes15@gmail.com>
2023-06-10 22:07:32 +0100

    saw AoE added but need polish

--- UnityProjects/VanHelsingProductionProject/Assets/Scripts/PlayerScripts/PlayerMonoBehaviours/SawArmController.cs
+++ UnityProjects/VanHelsingProductionProject/Assets/Scripts/PlayerScripts/PlayerMonoBehaviours/SawArmController.cs
@@ -1,9 +1,12 @@
+using PlayerScripts.Enums;
 using UnityEngine;
 
 public class SawArmController : MonoBehaviour
 {
     
     [SerializeField] private float _attackCooldownDuration = 0.5f;
+    [SerializeField] private GameObject _sawAreaOfEffect;
+    [SerializeField] private Transform _areaOfEffectInstantiationPoint;
     private float _atkCoolDownTimer = 0;
     public float AttackCooldownDuration => _attackCooldownDuration;
     public float AtkCoolDownTimer => _atkCoolDownTimer;
@@ -35,6 +38,27 @@ public class SawArmController : MonoBehaviour
     {
         if (!(_atkCoolDownTimer <= 0)) return;
         _sawArmAnimator.SetTrigger(ShootAnimatorParameter);
+        
+        // AoE Instantiation
+        GameObject AoE = Instantiate(_sawAreaOfEffect);
+        
+        // AoE positioning
+        AoE.transform.position = _areaOfEffectInstantiationPoint.transform.position;
+       
+        // AoE rotation
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
+        Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        AoE.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        
+        // sets the sprite flip to match
+        if (_playerController.CurrentFacingDirection == FacingDirection.Left)
+            AoE.GetComponent<SpriteRenderer>().flipY = true;
+        
+        // Cooldown Reset
         _atkCoolDownTimer = _attackCooldownDuration;
     }
 
