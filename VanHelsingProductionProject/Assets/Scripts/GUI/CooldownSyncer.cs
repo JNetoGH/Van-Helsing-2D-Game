8b5e64d39f56bcf1d508fca223132f810544dc85
JNeto <joaomendes15@gmail.com>
2023-07-21 08:04:17 +0100

    Ui bug fixed

--- VanHelsingProductionProject/Assets/Scripts/GUI/CooldownSyncer.cs
+++ VanHelsingProductionProject/Assets/Scripts/GUI/CooldownSyncer.cs
@@ -29,10 +29,19 @@ public class CooldownSyncer : MonoBehaviour
     void Update()
     {
         _crossbowCooldownSlider.value = _crossbowArmController.ShotCooldownTimer;
+        _crossbowCooldownSlider.GetComponentInChildren<Image>().enabled = ! (_crossbowCooldownSlider.value <= 0);  // solves a bug, when it's zero a red line is kept on the bottom
+        
         _sawCooldownSlider.value = _sawArmController.AtkCooldownTimer;
+        _sawCooldownSlider.GetComponentInChildren<Image>().enabled = ! (_sawCooldownSlider.value <= 0); 
+        
         _dashCooldownSlider.value = _playerController.DashCooldownTimer;
+        _dashCooldownSlider.GetComponentInChildren<Image>().enabled = ! (_dashCooldownSlider.value <= 0); 
+        
         if (_werewolfCooldownSlider is not null && _fourthFloorManager is not null)
+        {
             _werewolfCooldownSlider.value = _fourthFloorManager.WerewolfCooldownTimer;
+            _werewolfCooldownSlider.GetComponentInChildren<Image>().enabled = ! (_werewolfCooldownSlider.value <= 0); 
+        }
     }
     
 }
